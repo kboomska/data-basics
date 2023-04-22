4e218f8bf30a3e3aebe7662eac89a567e722802d
Aleksey Kuzmin <kboomska@gmail.com>
2023-04-22 11:54:54 +0300

    Lesson 53: Data Basics. File, Directory (final code)

--- file_directory/lib/widgets/file_example_widget_model.dart
+++ file_directory/lib/widgets/file_example_widget_model.dart
@@ -1,7 +1,46 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart' as pathProvider;
+
 import 'package:flutter/material.dart';
 
 class FileExampleWidgetModel extends ChangeNotifier {
-  void readFile() async {}
+  void readFile() async {
+    // pathProvider.getApplicationDocumentsDirectory();
+    // pathProvider.getApplicationSupportDirectory();
+    // pathProvider.getTemporaryDirectory();
+    // pathProvider.getLibraryDirectory();
+
+    final directory = await pathProvider.getApplicationDocumentsDirectory();
+
+    final filePath = '${directory.path}/test_file';
+    final file = File(filePath);
+    print(filePath);
+
+    // final isExist = await file.exists(); // false
+    await file.writeAsString('Hello Flutter!');
+
+    final isExist = await file.exists(); // true
+    print(isExist);
+
+    if (!isExist) {
+      await file.create();
+    }
+
+    final result = await file.readAsString();
+    print(result);
+
+    // Image.file(file); // Read an image
+
+    final stat = await file.stat();
+    print(stat);
+
+    print(directory.parent);
+    print(directory.uri);
+
+    final fileSinc = file.openWrite();
+    fileSinc.close(); // It's important to close the file!
+  }
 }
 
 class FileExampleWidgetProvider extends InheritedNotifier {
