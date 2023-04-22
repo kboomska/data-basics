7dd5421d3710ba79385d18d14eb413edbfdcb3b7
Aleksey Kuzmin <kboomska@gmail.com>
2023-04-22 17:22:55 +0300

    Lesson 55: Data Basics. Hive (initial code)

---
+++ hive_basics/lib/widgets/hive_example_model.dart
@@ -0,0 +1,8 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HiveExampleModel {
+  HiveExampleModel() {
+    Hive.initFlutter();
+  }
+  void doSome() {}
+}
---
+++ hive_basics/lib/widgets/hive_example_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:hive_basics/widgets/hive_example_model.dart';
+
+class HiveExampleWidget extends StatefulWidget {
+  const HiveExampleWidget({super.key});
+
+  @override
+  State<HiveExampleWidget> createState() => _HiveExampleWidgetState();
+}
+
+class _HiveExampleWidgetState extends State<HiveExampleWidget> {
+  final model = HiveExampleModel();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: model.doSome,
+            child: const Text('Press'),
+          ),
+        ),
+      ),
+    );
+  }
+}
