b677542d04e3c4814d4f472b4b331db6dbe4152f
Aleksey Kuzmin <kboomska@gmail.com>
2023-04-27 16:38:18 +0300

    Lesson 60: Data Basics. To Do List (keeping the balance of open boxes)

--- todo/lib/domain/data_provider/box_manager.dart
+++ todo/lib/domain/data_provider/box_manager.dart
@@ -5,6 +5,7 @@ import 'package:todo/domain/entity/task.dart';
 
 class BoxManager {
   static final BoxManager instance = BoxManager._();
+  final Map<String, int> _boxCounter = <String, int>{};
 
   BoxManager._();
 
@@ -17,6 +18,16 @@ class BoxManager {
   }
 
   Future<void> closeBox<T>(Box<T> box) async {
+    if (!box.isOpen) {
+      _boxCounter.remove(box.name);
+      return;
+    }
+    var count = _boxCounter[box.name] ?? 1;
+    count -= 1;
+    _boxCounter[box.name] = count;
+    if (count > 0) return;
+
+    _boxCounter.remove(box.name);
     await box.compact();
     await box.close();
   }
@@ -28,6 +39,14 @@ class BoxManager {
     int typeId,
     TypeAdapter<T> adapter,
   ) async {
+    if (Hive.isBoxOpen(boxName)) {
+      final count = _boxCounter[boxName] ?? 1;
+      _boxCounter[boxName] = count + 1;
+      return Hive.box(boxName);
+    }
+
+    _boxCounter[boxName] = 1;
+
     if (!Hive.isAdapterRegistered(typeId)) {
       Hive.registerAdapter(adapter);
     }
